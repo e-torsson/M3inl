461d640a9023f844e9042bb98dd4b09a40331e25
Eric <eric.torsson@outlook.com>
2025-01-07 18:05:44 +0100

    Grunduppgiften löst

--- M3inl.ino
+++ M3inl.ino
@@ -1,28 +1,63 @@
-#define BUTTON_PIN 4
+/*
+What you need to test:
+7 - wires
+1 - resistor
+1 - button
+1 - Obstacle Avoidance Sensor Module
+*/
+
+#define BUTTON_PIN_D 2 // Pin connected to the button (digital)
+#define PROX_PIN_D 7 // Pin connected to the prox sensor (digital)
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_D, INPUT_PULLUP);
+  pinMode(PROX_PIN_D, INPUT);
   Serial.begin(9600);
-
 }
 
-// Initial value of button (LED / Sensor OFF)
-int buttonValue = HIGH;
+int ledValue = HIGH; // Tracks LED state - HIGH = ON, LOW = OFF
+int lastButtonState = HIGH; // Checks the last state of the button
 
 void loop() {
-  if(digitalRead(BUTTON_PIN) == LOW){
+  // Read the current button state
+  int currentButtonState = digitalRead(BUTTON_PIN_D);
+
+  // Debounce logic
+  if(currentButtonState == LOW && lastButtonState == HIGH)
+  {
     Serial.println("BUTTON PRESSED");
-    if(buttonValue == HIGH){
+    if(ledValue == HIGH)
+    {
       Serial.println("LED OFF");
-      buttonValue = LOW;
+      ledValue = LOW; // Turn the LED OFF
     }
-    else{
+    else
+    {
       Serial.println("LED ON");
-      buttonValue = HIGH;
+      ledValue = HIGH; // Turn the LED ON
+    }
+    delay(100);
+  }
+
+  // Update button state
+  lastButtonState = currentButtonState;
+
+  // Update LED state
+  digitalWrite(LED_BUILTIN, ledValue);
+
+  // Only check if LED is on
+  if(ledValue == HIGH)
+  {
+    int proxValue = digitalRead(PROX_PIN_D); // Read sensor value
+    if(proxValue == LOW)
+    {
+      Serial.println("Object detected");
+    }
+    else
+    {
+      Serial.println("No object detected");
     }
   }
-  digitalWrite(LED_BUILTIN, buttonValue);
-  delay(200);
-
+  delay(400);
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+<img src="docs/layout.png" alt="layout">
